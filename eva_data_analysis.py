9ac617659ad08434e5af3300aab4d8b112456693
DafniLettos <daphnelettos@gmail.com>
2026-02-24 15:49:49 +0000

    added comments to code

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -3,20 +3,21 @@ import pandas as pd
 '''
 This is a multiline comment
 '''
-
 # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r', encoding='ascii')
 output_file = open('./eva-data.csv', 'w', encoding='utf-8')
 graph_file = './cumulative_eva_graph.png'
 
+# Read-in and format input json
 eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
 eva_df['eva'] = eva_df['eva'].astype(float)
 eva_df.dropna(axis=0, subset=['duration', 'date'], inplace=True)
 
+# Export to csv and sort by date
 eva_df.to_csv(output_file, index=False, encoding='utf-8')
-
 eva_df.sort_values('date', inplace=True)
 
+# Plot cumulative time spent in space over time
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
