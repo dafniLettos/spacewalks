7b5532c2d453deecb540c45491ab7f307d40fc21
DafniLettos <daphnelettos@gmail.com>
2026-02-25 11:33:55 +0000

    Added functions for crew info and tests

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import sys # In-built Pythpn library
-
+import sys # In-built Python library
+import re
 
 # MAIN code
 def main(input_file, output_file,graph_file):
     print("--START--")
     # Read the data from JSON file
     df = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    df = add_crew_size_column(df)
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(df, output_file)
@@ -19,6 +22,38 @@ def main(input_file, output_file,graph_file):
     plot_cumulative_time_in_space(df, graph_file)
 
     print("--END--")
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 
 def read_json_to_dataframe(input_file):
--- tests/test_eva_data_analysis.py
+++ tests/test_eva_data_analysis.py
@@ -1,4 +1,4 @@
-from eva_data_analysis import text_to_duration
+from eva_data_analysis import text_to_duration, calculate_crew_size
 import pytest
 
 #def test_text_to_duration_integer():
@@ -14,6 +14,7 @@ def test_text_to_duration_integer():
     assert text_to_duration("10:00") == 10
 # Expect no output if everything went well
 
+
 def test_text_to_duration_float():
     """
     Test that text_to_duration returns expected ground truth values for typical durations with a non zero minutes component
@@ -25,3 +26,22 @@ def test_text_to_duration_float():
 #test_text_to_duration_integer()
 #test_text_to_duration_float()
 
+def test_calculate_crew_size():
+    """
+    Test that calculate_crew_size returns expected size for typical crew values
+    """
+    actual_result = calculate_crew_size("Dafni Lettos;")
+    expected_result = 1
+    assert actual_result == expected_result
+
+    actual_result = calculate_crew_size("Dafni Lettos; Alex Taylor;")
+    expected_result = 2
+    assert actual_result == expected_result
+
+
+def test_calculate_crew_size_edge_cases():
+    """
+    Test that calculate_crew_size returns expected size for atypical crew values (edge case were crew is empty string)
+    """
+    actual_result = calculate_crew_size("")
+    assert actual_result == None
